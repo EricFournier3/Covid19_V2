b68c80858fd8b872ca9e49a9973828b009cc4de4
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-05-11 12:03:01 -0400

    ef

--- NextStrainFiles/tools/ImportGenomeCenterSeq.sh
+++ NextStrainFiles/tools/ImportGenomeCenterSeq.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+<<COM
+Eric Fournier 2020-05-11
+Import Covid-19 assemblies from ComputeCanada:Beluga
+
+COM
+
+
 green_message="\e[32m"
 white_message="\e[39m"
 
-base_dir_slbio="/data/Runs/SARS-CoV-2/GenomeCenterSeq/FinalRelease"
+base_dir_slbio="/data/Runs/SARS-CoV-2/GenomeCenterSeq/"
+
+out_seq_slbio=${base_dir_slbio}"FinalRelease/"
+log_slbio="${out_seq_slbio}BelugaImport.log"
 
 beluga_server="fournie1@beluga.computecanada.ca"
 beluga_seq_dir="/home/fournie1/test"
@@ -12,10 +22,14 @@ slbio_user=$(whoami)
 beluga_pass_file="/home/${slbio_user}/compute_canada_pass.txt"
 read beluga_pw < ${beluga_pass_file}
 
-scp_cmd="sshpass -p ${beluga_pw}  scp -r ${beluga_server}:${beluga_seq_dir} ${base_dir_slbio}"
+scp_cmd="sshpass -p ${beluga_pw}  scp -r ${beluga_server}:${beluga_seq_dir} ${out_seq_slbio}"
 
-echo -e "${green_message}INFO: " "Begin import from ${beluga_server}:${beluga_seq_dir}"
+echo -e "Begin import from ${beluga_server}:${beluga_seq_dir}\t$(date "+%Y-%m-%d @ %H:%M$S")" >> ${log_slbio}
+
+#echo -e "${green_message}INFO: " "Begin import from ${beluga_server}:${beluga_seq_dir}"
 eval ${scp_cmd}
-echo -e "${green_message}INFO: " "End import from ${beluga_server}:${beluga_seq_dir}"
+#echo -e "${green_message}INFO: " "End import from ${beluga_server}:${beluga_seq_dir}"
 
-echo -e "${white_message}"
+echo -e "End import from ${beluga_server}:${beluga_seq_dir}\t$(date "+%Y-%m-%d @ %H:%M$S")\n" >> ${log_slbio}
+
+#echo -e "${white_message}"
