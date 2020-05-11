97ca2587a430f843f0171d2fa43512fd07e99b8f
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-05-11 13:16:43 -0400

    ef

--- NextStrainFiles/tools/ImportGenomeCenterSeq.sh
+++ NextStrainFiles/tools/ImportGenomeCenterSeq.sh
@@ -10,12 +10,18 @@ COM
 green_message="\e[32m"
 white_message="\e[39m"
 
+lspq_data_nextstrain_dir="/data/Applications/GitScript/Covid19_V2/NextStrainFiles/data/lspq/"
+
+#TODO Change fasta name
+lspq_seq_for_nextstrain=${lspq_data_nextstrain_dir}"sequences_temp.fasta"
+
 base_dir_slbio="/data/Runs/SARS-CoV-2/GenomeCenterSeq/"
 
 out_seq_slbio=${base_dir_slbio}"FinalRelease/"
 log_slbio="${out_seq_slbio}BelugaImport.log"
 
 beluga_server="fournie1@beluga.computecanada.ca"
+#TODO Change path
 beluga_seq_dir="/home/fournie1/test"
 
 slbio_user=$(whoami)
@@ -27,9 +33,17 @@ scp_cmd="sshpass -p ${beluga_pw}  scp -r ${beluga_server}:${beluga_seq_dir} ${ou
 echo -e "Begin import from ${beluga_server}:${beluga_seq_dir}\t$(date "+%Y-%m-%d @ %H:%M$S")" >> ${log_slbio}
 
 #echo -e "${green_message}INFO: " "Begin import from ${beluga_server}:${beluga_seq_dir}"
-eval ${scp_cmd}
+#TODO remove comment
+#eval ${scp_cmd}
 #echo -e "${green_message}INFO: " "End import from ${beluga_server}:${beluga_seq_dir}"
 
-echo -e "End import from ${beluga_server}:${beluga_seq_dir}\t$(date "+%Y-%m-%d @ %H:%M$S")\n" >> ${log_slbio}
+echo -e "End import from ${beluga_server}:${beluga_seq_dir}\t$(date "+%Y-%m-%d @ %H:%M$S")" >> ${log_slbio}
 
 #echo -e "${white_message}"
+
+echo -e "Begin concat fasta to ${lspq_seq_for_nextstrain} \t$(date "+%Y-%m-%d @ %H:%M$S")" >> ${log_slbio}
+#TODO remove comment
+#cat "${out_seq_slbio}$(basename ${beluga_seq_dir})/"*".fasta"  > ${lspq_seq_for_nextstrain}
+echo -e "End concat fasta to ${lspq_seq_for_nextstrain} \t$(date "+%Y-%m-%d @ %H:%M$S")\n" >> ${log_slbio}
+
+
