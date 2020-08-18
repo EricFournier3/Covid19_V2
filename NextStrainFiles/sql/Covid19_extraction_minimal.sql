08f3c263bcfd8e5d87175306b9b485fa707e90aa
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-08-18 15:42:18 -0400

    ef

--- NextStrainFiles/sql/Covid19_extraction_minimal.sql
+++ NextStrainFiles/sql/Covid19_extraction_minimal.sql
@@ -1,5 +1,5 @@
 select distinct FOLDERNO as NUMERO_SGIL , to_char(cr.DATE_COLLECTED,'YYYY-MM-DD') as SAMPLED_DATE, cr.LAST_NAME as NOM, cr.FIRST_NAME as PRENOM, to_char(cr.BIRTH_DATE,'YYYY-MM-DD') as DATE_NAISS, 
-PID,(adress || ' ' || adress_a || ', '|| CITY ||', QC, Canada') as CH_ADRESS, COMPNAME as CH_NAME
+PID,REPLACE(REPLACE(adress || ' ' || adress_a || ', '|| CITY ||', QC, Canada',CHR(10),' '),CHR(13),' ') as CH_ADRESS, COMPNAME as CH_NAME
 from CENTRALRECEIVING cr inner join RASCLIENTS rc on rc.RASCLIENTID = cr.RASCLIENTID where (cr.PANEL_LIST like '2019-nCoV%' or cr.PANEL_LIST like '%COVID-19 - Confirmation%' or  cr.PANEL_LIST like '%GeneXpert SARS-CoV-2%' or
 cr.PANEL_LIST like '%COVID-19 - Soustraitant%' or cr.PANEL_LIST like '%COVID-19 - Surveillance WGS%') and rc.RASCLIENTID
 not in ('LSPQCEC','LSPQCIC','LSPQF','LSPQP','LSPQV') AND FOLDERNO  in (SELECT FOLDERNO from RESULTS res where (res.TESTCODE in (2705) and res.ANALYTE='Génotype') or (res.TESTCODE in (2685,2689)
