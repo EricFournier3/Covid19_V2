517834d3588f9dbaedfe90a2f120ff914bf963b0
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-12-19 22:46:28 -0500

    ef

--- NextStrainFiles/tools/ParseVCF_v2.py
+++ NextStrainFiles/tools/ParseVCF_v2.py
@@ -13,22 +13,27 @@ import argparse
 import glob
 
 vcf_basedir = "/home/foueri01@inspq.qc.ca/temp/TEST_GET_FASTA_FOR_NEXTSTRAIN/OUT/VCF/"
-vcf_outdir = os.path.join(vcf_basedir,"PARSED")
-
+parsed_outdir = os.path.join(vcf_basedir,"PARSED")
+parsed_outfile = os.path.join(parsed_outdir,"Variant_20200201_20201218.xlsx")
 
 vcf_infiles = glob.glob(vcf_basedir + "/*.vcf")
 
 #print(vcf_infiles)
 
+spec_mut_dict = {}
+
 for vcf_file in vcf_infiles:
+    spec = os.path.basename(vcf_file).split('.')[0].split('_')[0]
+    #print(vcf_file, " => ",spec)
+    spec_mut_dict[spec] = []
     header_read = False
     is_header = {}
     is_nanopore = False
     line_nb = 0
-    print(vcf_file)
+    #print(vcf_file)
 
     with open(vcf_file) as vcff:
-        aa_mut_list =  []
+        
         for line in vcff:
             line_nb += 1 
             is_header = False
@@ -64,72 +69,21 @@ for vcf_file in vcf_infiles:
                     #print(">>>> ",aa_change_first_annotation)
                     gene_name = aa_change_first_annotation[3]
                     aa_mut = aa_change_first_annotation[10][2:]
-                    aa_mut_list.append(gene_name + ":" + aa_mut) 
+                    spec_mut_dict[spec].append(gene_name + ":" + aa_mut) 
 
-        print(aa_mut_list)     
-        print("********************************************")
-                
+        #print(spec_mut_dict[spec])     
+        #print("********************************************")
+     
+mut_df = pd.DataFrame(columns=['# Requête',"AA_MUTATIONS"])
+envois_gq_df = pd.read_excel("/data/Databases/CovBanQ_Epi/LISTE_ENVOIS_GENOME_QUEBEC/ListeEnvoisGenomeQuebec_test_variant.xlsx",sheet_name=0) 
 
+for spec,mut in spec_mut_dict.items():
+    #print(spec, " : ",str(mut))
+    mut_df = mut_df.append({'# Requête':spec,'AA_MUTATIONS':str(mut)},ignore_index=True)
+    mut_df['# Requête'] = mut_df['# Requête'].str.upper()
 
-exit(0)
-
-vcf_file = "/data/PROJETS/COVID-19_Beluga/VCF/JUS-V5290618.sorted.filtered.primerTrim.annotateTEST.vcf"
-out_test = "/data/PROJETS/COVID-19_Beluga/VCF/variant_test.xlsx"
-
-
-header_read = False
-is_header = False
-
-mut = {}
-
-with open(vcf_file) as vcff:
-    for line in vcff:
-
-        is_header = False
-
-        if re.search(r'^#CHROM\t',line):
-            header_read = True
-            is_header = True
-        if header_read and not is_header:
-            nuc_change = ""
-            aa_change_set = set([])
-
-            split_line = line.split('\t')
-            nuc_change_pos = split_line[1]
-            nuc_ref = split_line[3]
-            nuc_mut = split_line[4]
-            nuc_change = nuc_ref + nuc_change_pos + nuc_mut
-            nuc_change_qual = split_line[6]
-
-            if nuc_change_qual != "PASS":
-                continue
-
-            mut[nuc_change] = aa_change_set
-
-            aa_change_annotations = split_line[7]
-            aa_change_annotations = aa_change_annotations.split(',')
-
-            for aa_change_annotation in aa_change_annotations:
-                aa_change_annotation_split = aa_change_annotation.split('|')
-                nuc_mut_type = aa_change_annotation_split[1]
-                if (len(aa_change_annotation_split[15]) == 0):
-                    if (nuc_mut_type == 'missense_variant') :
-                        aa_change = aa_change_annotation_split[3] + ":" + aa_change_annotation_split[10][2:]
-                    else:
-                        aa_change = nuc_mut_type
-                    aa_change_set.add(aa_change)
-    
-nuc_mut_string = ""
-aa_mut_string = ""
-        
-for nuc_mut in mut.keys():
-    nuc_mut_string = nuc_mut_string + nuc_mut + "|"
-    for aa_mut in mut[nuc_mut]:
-        aa_mut_string = aa_mut_string + aa_mut + ","
-    
-    aa_mut_string = aa_mut_string.strip(',')
-    aa_mut_string = aa_mut_string + "|"
-
-pd_df = pd.DataFrame({"NUC_VARIANT":[nuc_mut_string],"AA_VARIANT":[aa_mut_string]})
-pd_df.to_excel(out_test,sheet_name='Sheet1',index=False)
+final_mut_df = pd.merge(mut_df,envois_gq_df,how='inner',on = ['# Requête'])
+final_mut_df = final_mut_df[['# Requête','Nom','Prénom','Date de naissance','NAM','AA_MUTATIONS']]
+#print(final_mut_df)
+final_mut_df.to_excel(parsed_outfile,sheet_name='Sheet1')
 
