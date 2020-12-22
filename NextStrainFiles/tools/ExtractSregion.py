5d1ea6a07423ef567da20f1971f3bfdfb72762b6
Éric Fournier <foueri01@inspq.qc.ca@slbio00d.inspq.qc.ca>
2020-12-21 21:36:59 -0500

    ef

--- NextStrainFiles/tools/ExtractSregion.py
+++ NextStrainFiles/tools/ExtractSregion.py
@@ -1,6 +1,7 @@
 from Bio import AlignIO
 from Bio import SeqIO
 import argparse
+import math
 
 parser = argparse.ArgumentParser(description="")
 parser.add_argument("--alignment",required=True,help="FASTA alignment")
@@ -16,3 +17,19 @@ for rec in AlignIO.read(args.alignment,'fasta'):
     #print("S Length ",len(rec_s.seq))
 
 SeqIO.write(rec_s_list,args.output_s_region,'fasta')
+
+nb_total = len(rec_s_list)
+print("TOTAL ",nb_total)
+
+nb_var = 0
+
+for rec in AlignIO.read(args.output_s_region,'fasta'):
+    del_region = rec.seq[202:208]
+    if 'N' in del_region:
+        nb_var += 1
+
+print("NB VAR ",nb_var)
+
+frac = format((float(nb_var) / float(nb_total)) * 100,'.2f')
+print("FRAC ",frac)
+
